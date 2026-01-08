c2f1f2e100674fafb18c1cdb031f1ab26ea6fec0
carlosbonadeo <carlosbonadeo@skyscanner.net>
2026-01-08 23:45:08 +0000

    Legend

--- src/render/legend.ts
+++ src/render/legend.ts
@@ -3,7 +3,7 @@ import type { TranslationKey } from '../types/i18n';
 
 function createLegendItem(className: string, labelKey: TranslationKey) {
   const div = document.createElement('div');
-  div.innerHTML = `<span class="day weekday ${className}">1</span> <span>${t(labelKey)}</span>`;
+  div.innerHTML = `<span class="day weekday ${className}"></span> <span>${t(labelKey)}</span>`;
   return div;
 }
 
